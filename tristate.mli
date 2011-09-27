2f9bbc407d9f5c1659c24e0602d2a10c8b1b5699
Martin Jambon <martin@mjambon.com>
2011-09-26 23:18:58 -0700

    Fix

--- tristate.mli
+++ tristate.mli
@@ -2,6 +2,6 @@ type tristate
 
 type view = Positive | Zero | Negative
 
-val boost : t -> int -> unit
-val next : t -> unit
-val view : t -> view
+val boost : tristate -> int -> unit
+val next : tristate -> unit
+val view : tristate -> view
