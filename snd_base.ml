d54f0ca362363625be12030111a580b967bd7d09
Martin Jambon <martin@mjambon.com>
2011-11-10 23:38:54 -0800

    Added shorter and longer notes, and a blank.

--- snd_base.ml
+++ snd_base.ml
@@ -3,15 +3,24 @@ open Snd_gen
 
 let wedge = Rel ("wedge", (fun x -> 1. -. 2. *. abs_float (x -. 0.5)))
 
+let tu = 0.25 (* time unit *)
+
 let base_notes =
-  Array.to_list (
-    Array.init 8 (
-      fun i ->
-        let freq = 440. *. (1. +. log (float (i + 1)) /. log 8.) in
-        let name = sprintf "%.1fHz" freq in
-        name, sy wedge (atom 0.25 name (sine freq))
+  List.flatten (
+    Array.to_list (
+      Array.init 8 (
+        fun i ->
+          let freq = 440. *. (1. +. log (float (i + 1)) /. log 8.) in
+          let name = sprintf "%.1fHz" freq in
+          [ 
+            name, sy wedge (atom tu name (sine freq));
+            name ^ "-", sy wedge (atom (0.5 *. tu) name (sine freq));
+            name ^ "+", sy wedge (atom (2. *. tu) name (sine freq));
+          ]
+      )
     )
   )
+  @ [ "blank", blank tu ]
 
 let wavetable =
   let tbl = Hashtbl.create 20 in
